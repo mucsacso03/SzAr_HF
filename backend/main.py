d4b06d6299a27b58fde2db9f15b7ec2377bfd294
David <davidbardos97@gmail.com>
2021-11-27 18:00:49 +0100

    ID unique tarolasa leaderboard sort elso 10 visszaadasa

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,9 @@
 import json
+import string
 import threading
 import time
 from datetime import datetime
+from random import SystemRandom
 
 from flask import Flask, jsonify, request
 from flask_cors import CORS
@@ -73,7 +75,26 @@ def game_deleting_thread(id):
 
 class Leaderboard(Resource):
     def get(self):
+        # entry1 = Leaderboard_Entry(username='username', score=1)
+        # entry2 = Leaderboard_Entry(username='rname', score=5)
+        # entry3 = Leaderboard_Entry(username='username', score=3)
+        # entry4 = Leaderboard_Entry(username='username', score=30)
+        # entry5 = Leaderboard_Entry(username='username', score=21)
+        # entry6 = Leaderboard_Entry(username='username', score=7)
+        # entry7 = Leaderboard_Entry(username='username', score=13)
+        # entry8 = Leaderboard_Entry(username='username', score=18)
+        # db.session.add(entry1)
+        # db.session.add(entry2)
+        # db.session.add(entry3)
+        # db.session.add(entry4)
+        # db.session.add(entry5)
+        # db.session.add(entry6)
+        # db.session.add(entry7)
+        # db.session.add(entry8)
+        # db.session.commit()
         q = Leaderboard_Entry.query.all()
+        q.sort(key=lambda x: x.score, reverse=False)
+        q = q[:10]
         files = {}
         num = 1
 
@@ -93,14 +114,14 @@ class Move(Resource):
             x = int(content['x'])
             y = int(content['y'])
             id = int(content['id'])
-
+            print('ACTIVE GAMES:', len(Games))
             print("REQUEST:")
             print(request.get_json())
 
-            G = [x for x in Games if x.id == id][0]
-            victory, robot_v = G.move(x, y)
+            game = find_game_in_games_list(id)
+            victory, robot_v = game.move(x, y)
             if victory:
-                username = G.username
+                username = game.username
                 delete_game_instance(id)
                 return jsonify({"won": username})
 
@@ -109,7 +130,7 @@ class Move(Resource):
                 return jsonify({"won": 'robot'})
             else:
                 return jsonify({'won': '',
-                                'field': G.get_field()})
+                                'field': game.get_field()})
 
         except Exception as e:
             print("ERROR")
@@ -118,22 +139,33 @@ class Move(Resource):
             return str(404)
 
 
+def generate_id():
+    while True:
+        game_id = ''.join(SystemRandom().choice(string.digits) for _ in range(4))
+        if find_game_in_games_list(game_id) is None:
+            return int(game_id)
+
+
+
 class NewGame(Resource):
     def post(self):
         content = request.get_json()
         username = content['username']
         global Games
-        # id = ''.join(SystemRandom().choice(string.digits) for _ in range(2))
-        id = 123  # TODO: only for debug
-        Games.append(game_instance(int(id), username))
-
-        threading.Thread(target=game_deleting_thread, args=[id], daemon=True).start()
+        game_id = generate_id()
+        # game_id = 123  # TODO: only for debug
+        Games.append(game_instance(int(game_id), username))
+        threading.Thread(target=game_deleting_thread, args=[game_id], daemon=True).start()
 
         # Itt is vissszaküldjük a pályát, hogy egyszerűbb dolgunk legyen
-        game = [x for x in Games if x.id == id][0]
-        return (game.get_field())
-        # return json.dumps({"data" : game.get_field()})
-        return json.dumps(int(id))  # ezt is, meg a fentit is kéne
+        game = find_game_in_games_list(game_id)
+
+        return jsonify({'field': game.get_field(),
+                        'game_id': int(game_id)})
+
+        # return (game.get_field())
+        # # return json.dumps({"data" : game.get_field()})
+        # return json.dumps(int(game_id))  # ezt is, meg a fentit is kéne
 
 
 api.add_resource(Move, "/move")
